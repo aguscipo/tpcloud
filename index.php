412d7598c2a448268ef18fda9319e35b4939a9c3
aguscipo <agus_cipo_lp@hotmail.com>
2016-10-07 11:17:33 -0300

    Example drive

--- index.php
+++ index.php
@@ -1,37 +1,17 @@
-<!doctype html>
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+session_start();
 
-<html lang="es">
-<head>
-  <meta charset="utf-8">
+$client = new Google_Client();
+$client->setAuthConfig('client_secrets.json');
+$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 
-  <title>DSSD Cloud</title>
-  <meta name="google-signin-client_id" content="1009844009027-85kivvsggdlcja37umpgige7djrvc4us.apps.googleusercontent.com">
-  <script src="https://apis.google.com/js/platform.js" async defer></script>
-<!--  <link rel="stylesheet" href="css/styles.css?v=1.0">-->
-</head>
-
-<body>
-
-  <div class="g-signin2" data-onsuccess="onSignIn"></div>
-  <a href="" onclick="signOut();">Sign out</a>
-
-
-  <script>
-    function onSignIn(googleUser) {
-      console.log ("HOLAAAAAAAAAAA");
-      var profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail());
-    }
-
-    function signOut() {
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
-        console.log('User signed out.');
-      });
-    }
-  </script>
-</body>
-</html>
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client->setAccessToken($_SESSION['access_token']);
+  $drive_service = new Google_Service_Drive($client);
+  $files_list = $drive_service->files->listFiles(array())->getItems();
+  echo json_encode($files_list);
+} else {
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
