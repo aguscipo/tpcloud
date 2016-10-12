a359dca14094f215fc401dd7cbe19310f4afcf89
aguscipo <agus_cipo_lp@hotmail.com>
2016-10-12 09:51:46 -0300

    sign in

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
-<?php
-require_once __DIR__.'/vendor/autoload.php';
-session_start();
-
-$client = new Google_Client();
-$client->setAuthConfig('client_secrets.json');
-$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-
-if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-  echo "LA CONCHA DE TU MADRE";
-  $client->setAccessToken($_SESSION['access_token']);
-  $drive_service = new Google_Service_Drive($client);
-  $files_list = $drive_service->files->listFiles(array())->getItems();
-  echo json_encode($files_list);
-} else {
-  echo "LA CONCHA DE TU ELSE HERMANA";
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-}
+<html lang="es">
+  <head>
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="354327124533-7sm4ore9l6bikcfc4vtlc5l25nem6je0.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+  </head>
+  <body>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+      };
+    </script>
+  </body>
+</html>
