3a6f6746d316330331c9a5a3f418000b8d628bda
aguscipo <agus_cipo_lp@hotmail.com>
2016-10-12 11:26:37 -0300

    sign-in

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
   </head>
   <body>
     <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <div id="name"> </div>
+
+     <img id="profile-image" alt="Profile image" height="100" width="100">
     <script>
       function onSignIn(googleUser) {
         // Useful data for your client-side scripts:
         var profile = googleUser.getBasicProfile();
+        document.getElementById("name").innerText = "Bienvenido " + profile.getName();
+        document["profile-image"].src = profile.getImageUrl();
         console.log("ID: " + profile.getId()); // Don't send this directly to your server!
         console.log('Full Name: ' + profile.getName());
         console.log('Given Name: ' + profile.getGivenName());
