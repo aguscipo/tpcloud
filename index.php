60033eb5380fb0f105894316f7700fa739eba504
aguscipo <agus_cipo_lp@hotmail.com>
2016-10-12 12:37:25 -0300

    Add sign out

--- index.php
+++ index.php
@@ -30,13 +30,16 @@
         console.log("Email: " + profile.getEmail());
       };
 
-      function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-          console.log('User signed out.');
-          console.log(auth2);
-        });
-      }
     </script>
+    <script>
+
+          function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+              console.log('User signed out.');
+              console.log(auth2);
+            });
+          }
+  </script>
   </body>
 </html>
